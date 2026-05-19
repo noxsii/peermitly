af15670390c7089db12ef3e762f8e43393a3275e
dennisschirra <d.schirra@parkefficient.de>
2026-05-19 15:16:19 +0200

    feat: update font styles and color variables in CSS for improved design consistency

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,10 +2,10 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    @vite(['resources/css/app.css', 'resources/js/app.ts'])
-
     <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=akshar:300,400,500,700" rel="stylesheet" />
+    <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700" rel="stylesheet" />
+
+    @vite(['resources/css/app.css', 'resources/js/app.ts'])
 
     @routes
     <x-inertia::head />
