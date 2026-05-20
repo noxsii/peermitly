5b34e0f6b59a69088de9e884124a8e6964bb0ec7
dennisschirra <d.schirra@parkefficient.de>
2026-05-20 14:01:44 +0200

    feat: integrate Dedoc Scramble for OpenAPI documentation generation and configure API settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,12 +4,30 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+use Dedoc\Scramble\Scramble;
+use Illuminate\Routing\Route;
 
 final class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
         //
+    }
+
+    public function boot(): void
+    {
+        Scramble::configure()
+            ->routes(function (Route $route) {
+                return Str::startsWith($route->uri, 'api/');
+            })
+            ->withDocumentTransformers(function (OpenApi $openApi) {
+                $openApi->secure(
+                    SecurityScheme::http('bearer')
+                );
+            });
     }
 }
