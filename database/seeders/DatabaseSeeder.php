5db938a5ad1a90776ee8ef200b787bdf8578dac2
dennisschirra <d.schirra@parkefficient.de>
2026-05-20 14:36:57 +0200

    feat: add factories and seeder for license key management, including customer and product associations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 final class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        //
+        $this->call(LicenseKeyTypeSeeder::class);
     }
 }
