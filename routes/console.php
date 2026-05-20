bac6624bf5b84328fbcbd356237fa6252c401710
dennisschirra <d.schirra@parkefficient.de>
2026-05-20 16:15:39 +0200

    feat: add command to expire inactive license keys with scheduled execution

--- routes/console.php
+++ routes/console.php
@@ -2,4 +2,9 @@
 
 declare(strict_types=1);
 
-//
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::command('license-keys:expire')
+    ->hourly()
+    ->withoutOverlapping()
+    ->onOneServer();
